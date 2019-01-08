ca0fb3e5c35be905e0bae23524ca6154d453589a
Bernardus Jeffrey Darmawan <bernardusjeffrey747@gmail.com>
2019-01-08 22:39:38 +0700

    edited upload file to url, acquire response, and switch displays

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,15 @@
 $(function(){
 
+    const url = 'process.php';
+    const form = document.querySelector('form');
+
+    var predictOnClick = goToPredict();
+    var imageLink = "";
+
+    var post_response = "";
     var ul = $('#upload ul');
 
-    $('#drop a').click(function(){
+    $('#browse').click(function(){
         // Simulate a click on the file input button
         // to show the file browser dialog
         $(this).parent().find('input').click();
@@ -17,10 +24,15 @@ $(function(){
         // This function is called when a file is added to the queue;
         // either via the browse button, or via drag/drop:
         add: function (e, data) {
+            console.log("yey" + data.files[0]);
+
+            if($('#showimg li')) {
+                console.log("print this 2");
+                $('#showimg li').remove();
+            }
 
             var tpl = $('<li class="working"><input type="text" value="0" data-width="48" data-height="48"'+
                 ' data-fgColor="#0788a5" data-readOnly="1" data-bgColor="#3e4043" /><p></p><span></span></li>');
-
             // Append the file name and file size
             tpl.find('p').text(data.files[0].name)
                          .append('<i>' + formatFileSize(data.files[0].size) + '</i>');
@@ -33,19 +45,28 @@ $(function(){
 
             // Listen for clicks on the cancel icon
             tpl.find('span').click(function(){
-
-                if(tpl.hasClass('working')){
-                    jqXHR.abort();
-                }
-
-                tpl.fadeOut(function(){
-                    tpl.remove();
-                });
-
+                // data.submit();
             });
 
             // Automatically upload the file once it is added to the queue
-            var jqXHR = data.submit();
+            //var jqXHR = data.submit();
+
+            data.submit();
+            // upload and wait for further response
+            // var formData = new FormData();
+            // formData.append(data.files[0].name, data.files[0]);
+            //
+            // var xhr = new XMLHttpRequest();
+            // xhr.open("POST", "myServletUrl");
+            // xhr.send(formData);
+            //
+            // xhr.onreadystatechange = processRequest;
+            //
+            // function processRequest(e) {
+            //     if (xhr.readyState == 4 && xhr.status == 200) {
+            //         // do what after success
+            //     }
+            // }
         },
 
         progress: function(e, data){
@@ -92,4 +113,74 @@ $(function(){
         return (bytes / 1000).toFixed(2) + ' KB';
     }
 
+    // document onload disable predict button
+    $(document).ready(function() {
+        $('#container-predict').hide();
+    });
+
+
+
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+
+        const files = document.querySelector('[type=file]').files;
+        const formData = new FormData();
+
+        for (let i = 0; i < files.length; i++) {
+            let file = files[i];
+
+            formData.append('files[]', file);
+        }
+
+        fetch(url, {
+            method: 'POST',
+            body: formData
+        }).then(response => {
+            console.log(response);
+
+            // what to do: enable predict
+            $('#predict').attr('onclick', predictOnClick);
+            imageLink = response.imageLink;
+
+        });
+    });
+
+
+
+
+
+    function goToPredict(){
+        // change display
+        $('#container-predict').slideUp();
+        //
+        //
+        // $('.jqueryOptions').slideUp();
+        // $('.jqueryOptions').removeClass('current-opt');
+        // $("." + $(this).val()).slideDown();
+        // $("." + $(this).val()).addClass('current-opt');
+
+    }
+
 });
+
+var kuda= function(data) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+        $('#2')
+            .attr('src', data)
+            .width(150)
+            .height(200);
+    };
+    console.log(data);
+    // reader.readAsDataURL(e.files[0]);
+    console.log("abc");
+}
+
+var somefunction = function(){
+
+};
+
+var changeContent = function(){
+    $()
+}
